0ba86ad8e530e09c052af8d7b7c98d530c61d587
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-03-16 12:53:29 +0300

    Implement RawData to Data conversion
    Change-Id: Icb82d30244aa92d2c2e23870b1684a0d13a8ce2f

--- src/streams.rs
+++ src/streams.rs
@@ -1,5 +1,6 @@
 use super::utils::*;
 use async_trait::async_trait;
+use std::marker::PhantomData;
 
 #[async_trait]
 pub trait Sender<RawData> {
@@ -17,4 +18,58 @@ pub trait MessageSender<SI: SeqId, Data> {
 
 pub trait MessageReceiver<SI: SeqId, Data> {
     async fn recv(&mut self) -> ResultMessage<SI, Data>;
+}
+
+pub trait MessageBuilder<SI: SeqId, Data, RawData> {
+    fn build(&self, msg: Message<SI, Data>) -> Result<RawData>;
+}
+
+pub trait MessageParser<SI: SeqId, Data, RawData> {
+    fn parse(&self, raw: RawData) -> ResultMessage<SI, Data>;
+}
+
+pub struct MessageStreamSender<BuilderT, SenderT, RawData> {
+    builder: BuilderT,
+    sender: SenderT,
+    phantom: PhantomData<RawData>,
+}
+
+impl<SI, Data, RawData, BuilderT, SenderT> MessageSender<SI, Data>
+    for MessageStreamSender<BuilderT, SenderT, RawData>
+where
+    SI: SeqId,
+    BuilderT: MessageBuilder<SI, Data, RawData>,
+    SenderT: Sender<RawData>,
+{
+    async fn send(&mut self, msg: Message<SI, Data>) -> Status {
+        self.sender.send(self.builder.build(msg)?).await
+    }
+}
+
+impl<BuilderT, SenderT, RawData> MessageStreamSender<BuilderT, SenderT, RawData> {
+    pub fn new(builder: BuilderT, sender: SenderT) -> Self {
+        Self {
+            builder,
+            sender,
+            phantom: <PhantomData<RawData> as Default>::default(),
+        }
+    }
+}
+
+pub struct MessageStreamReceiver<ParserT, ReceiverT, RawData> {
+    parser: ParserT,
+    reciever: ReceiverT,
+    phantom: PhantomData<RawData>,
+}
+
+impl<SI, Data, RawData, ParserT, ReceiverT> MessageReceiver<SI, Data>
+    for MessageStreamReceiver<ParserT, ReceiverT, RawData>
+where
+    SI: SeqId,
+    ParserT: MessageParser<SI, Data, RawData>,
+    ReceiverT: Receiver<RawData>,
+{
+    async fn recv(&mut self) -> ResultMessage<SI, Data> {
+        self.parser.parse(self.reciever.recv().await?)
+    }
 }
