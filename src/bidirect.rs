6bcf772b846855b83c016d8ae13b33ace044358d
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-04-04 18:07:40 +0300

    Implement get_notice_sender for Bidirect
    Change-Id: I4d28d41bea26bbd7b4e6bbedb355d646649a54f0

--- src/bidirect.rs
+++ src/bidirect.rs
@@ -264,10 +264,35 @@ async fn get_request_sender() {
     _ = sender.request(String::from("Hello world!"));
 }
 
+struct NoticeSenderImpl<Data> {
+    channel: mpsc::Sender<Data>,
+}
+
+#[async_trait]
+impl<Data: Send> NoticeSender<Data> for NoticeSenderImpl<Data> {
+    async fn notify(&mut self, notice: Data) -> Status {
+        self.channel.send(notice).await.unwrap();
+        Ok(())
+    }
+}
+
+#[tokio::test]
+async fn get_notice_sender() {
+    let mut bidir = Bidirect::<String>::new();
+    let mut sender = bidir.get_notice_sender();
+    _ = sender.notify(String::from("Hello world!"));
+}
+
 impl<'b, Data: 'b + Send, SI: SeqId> BidirectStream<'b, Data> for Bidirect<'_, Data, SI> {
     fn get_request_sender(&mut self) -> impl RequestSender<Data> + 'b {
         RequestSenderImpl {
             channel: self.request_sender_user.clone(),
+        }
+    }
+
+    fn get_notice_sender(&mut self) -> impl NoticeSender<Data> + 'b {
+        NoticeSenderImpl {
+            channel: self.notice_sender_user.clone(),
         }
     }
 }
