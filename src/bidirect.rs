e06e415f22d40de6fccfa33f9e2deb070a605352
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-04-04 18:41:26 +0300

    Implement get_request_processor for Bidirect
    Change-Id: I8a25a0028c208b0ea7d85eb223cd21353f97ed74

--- src/bidirect.rs
+++ src/bidirect.rs
@@ -286,6 +286,26 @@ async fn get_notice_sender() {
     _ = sender.notify(String::from("Hello world!"));
 }
 
+struct RequestProcessorImpl<Data> {
+    channel: mpsc::Receiver<RequestImp<Data>>,
+}
+
+impl<Data: Send> crate::RequestProcessor<Data> for RequestProcessorImpl<Data> {
+    fn next_request(&mut self) -> impl Future<Output = Result<impl Request<Data>>> + Send {
+        let req = self.channel.recv();
+        async { Ok(req.await.unwrap()) }
+    }
+}
+
+#[tokio::test]
+async fn get_request_processor() {
+    use crate::RequestProcessor;
+
+    let mut bidir = Bidirect::<String>::new();
+    let mut processor = bidir.get_request_processor();
+    _ = processor.next_request();
+}
+
 impl<'b, Data: Send + 'b, SI: SeqId> BidirectStream<'b, Data> for Bidirect<'_, Data, SI> {
     fn get_request_sender(&mut self) -> impl RequestSender<Data> + 'b {
         RequestSenderImpl {
@@ -297,5 +317,11 @@ impl<'b, Data: Send + 'b, SI: SeqId> BidirectStream<'b, Data> for Bidirect<'_, D
         NoticeSenderImpl {
             channel: self.notice_sender_user.clone(),
         }
+    }
+
+    fn get_request_processor(&mut self) -> impl crate::RequestProcessor<Data> + 'b {
+        let (tx, rx) = mpsc::channel(16);
+        self.request_processor = Some(tx);
+        RequestProcessorImpl { channel: rx }
     }
 }
