8aeefcfaa4e886c5a8a48fc0356152d082193623
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-09-15 11:41:26 +0300

    [utils] Introduce own Error type
    Own error type will allow to implement From trait for different types.
    
    Change-Id: Ia938612cb23d7537fbad2c7e0876333d49da4509

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,37 @@
 use num::{Num, Zero};
-use std::io;
+use std::{io, fmt};
+use tokio::{sync::mpsc::error::SendError, task::JoinError};
 
 // Top-level type aliases
-pub type Error = io::Error;
-pub type Result<T> = io::Result<T>;
+pub struct Error(pub io::Error);
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(fmt)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl Error {
+    pub fn new(kind: io::ErrorKind, what: &str) -> Self
+    {
+        Error(io::Error::new(kind, what))
+    }
+
+    pub fn pipe(what: &str) -> Self
+    {
+        Self(io::Error::new(io::ErrorKind::BrokenPipe, what))
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 // Reuse type aliases
 pub type Status = Result<()>;
@@ -28,11 +56,11 @@ impl<Data, SI: SeqId> Message<Data, SI> {
     }
 }
 
-pub trait SeqId: Zero + PartialEq + Ord + Copy {
+pub trait SeqId: Zero + PartialEq + Ord + Copy + fmt::Display + Send + 'static {
     fn inc(self) -> Self;
 }
 
-impl<T: Num + Ord + Copy> SeqId for T {
+impl<T: Num + Ord + Copy + Send + fmt::Display + 'static> SeqId for T {
     fn inc(self) -> Self {
         return self + Self::one();
     }
@@ -45,6 +73,26 @@ pub trait ErrorBuilder {
 
 impl<Data, SI: SeqId> ErrorBuilder for Message<Data, SI> {
     fn not_implemented(what: &str) -> Self {
-        Self::Err(io::Error::new(io::ErrorKind::Unsupported, what), None)
+        Self::Err(
+            Error(io::Error::new(io::ErrorKind::Unsupported, what)),
+            None)
+    }
+}
+
+impl<T> From<SendError<T>> for Error {
+    fn from(value: SendError<T>) -> Self {
+        Self::pipe(&value.to_string())
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(value: io::Error) -> Self {
+        Self(value)
+    }
+}
+
+impl From<JoinError> for Error {
+    fn from(value: JoinError) -> Self {
+            Self::pipe(&format!("Failed to join: {}", value))
     }
 }
